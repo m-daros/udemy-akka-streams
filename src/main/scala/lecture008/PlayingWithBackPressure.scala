3223f6cbb6a0f627657f78430e245b609b8301a0
massimo.daros <massimo.daros@external.cerved.com>
2021-01-06 16:49:52 +0100

    Renaming PlayingWithBackPressure and PlayingWithThrottling

--- src/main/scala/lecture008/PlayingWithBackPressure.scala
+++ src/main/scala/lecture008/PlayingWithBackPressure.scala
@@ -1,42 +1,42 @@
 package lecture008
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorLogging, ActorSystem}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink, Source}
 
-import scala.concurrent.duration.DurationInt
-
-object playingWithBackThrottling extends App {
+object PlayingWithBackPressure extends App {
 
   implicit val actorSystem = ActorSystem ( "lecture008" )
   implicit val actorMaterializer = ActorMaterializer ()
 
   val numbersSource = Source ( 1 to 1000 )
 
-  val flow1 = Flow [Int].map (number => {
+  val slowFlow1 = Flow [Int].map ( number => {
 
+    Thread.sleep ( 1000 )
     val result = number + 1
     println ( s"slowFlow1 - Processed number: $number, producing result:  $result" )
 
     result
   } )
 
-  val flow2 = Flow [Int].map (number => {
+  val slowFlow2 = Flow [Int].map ( number => {
 
+    Thread.sleep ( 1000 )
     val result = number * 2
     println ( s"slowFlow2 - Processed number: $number, producing result:  $result" )
 
     result
   } )
 
-  val sink = Sink.foreach [Int] (number => {
+  val slowSink = Sink.foreach [Int] ( number => {
 
+    Thread.sleep ( 1000 )
     println ( s"slowSink - Result: $number" )
   } )
 
-  numbersSource.throttle ( 2, 1 second )
-    .via ( flow1 ).async
-    .via ( flow2 ).async
-    .to ( sink )
+  numbersSource.via ( slowFlow1 ).async
+    .via ( slowFlow2 ).async
+    .to ( slowSink )
     .run ()
 }
--- src/main/scala/lecture008/PlayingWithBackThrottling.scala
+++ src/main/scala/lecture008/PlayingWithBackThrottling.scala
@@ -1,42 +1,42 @@
 package lecture008
 
-import akka.actor.{ActorLogging, ActorSystem}
+import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink, Source}
 
-object playingWithBackPressure extends App {
+import scala.concurrent.duration.DurationInt
+
+object PlayingWithBackThrottling extends App {
 
   implicit val actorSystem = ActorSystem ( "lecture008" )
   implicit val actorMaterializer = ActorMaterializer ()
 
   val numbersSource = Source ( 1 to 1000 )
 
-  val slowFlow1 = Flow [Int].map ( number => {
+  val flow1 = Flow [Int].map (number => {
 
-    Thread.sleep ( 1000 )
     val result = number + 1
     println ( s"slowFlow1 - Processed number: $number, producing result:  $result" )
 
     result
   } )
 
-  val slowFlow2 = Flow [Int].map ( number => {
+  val flow2 = Flow [Int].map (number => {
 
-    Thread.sleep ( 1000 )
     val result = number * 2
     println ( s"slowFlow2 - Processed number: $number, producing result:  $result" )
 
     result
   } )
 
-  val slowSink = Sink.foreach [Int] ( number => {
+  val sink = Sink.foreach [Int] (number => {
 
-    Thread.sleep ( 1000 )
     println ( s"slowSink - Result: $number" )
   } )
 
-  numbersSource.via ( slowFlow1 ).async
-    .via ( slowFlow2 ).async
-    .to ( slowSink )
+  numbersSource.throttle ( 2, 1 second )
+    .via ( flow1 ).async
+    .via ( flow2 ).async
+    .to ( sink )
     .run ()
 }
