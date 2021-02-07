8495439e6a62859fcba99c4caa44bfee79228b09
massimo.daros <massimo.daros@external.cerved.com>
2021-02-07 19:03:21 +0100

    In exercise for lecture018, added test cases related to failing source with restart policy with backoff

--- src/test/scala/lecture018/HandlingFailuresSpec.scala
+++ src/test/scala/lecture018/HandlingFailuresSpec.scala
@@ -1,17 +1,17 @@
 package lecture018
 
 import akka.Done
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.ActorSystem
 import akka.actor.Status.Failure
+import akka.pattern.pipe
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Flow, RestartSource, Sink, Source}
+import akka.stream.scaladsl.{Flow, Keep, RestartSource, Sink, Source}
 import akka.testkit.TestProbe
-import akka.util.Timeout
 import org.scalatest.GivenWhenThen
 import org.scalatest.featurespec.AnyFeatureSpec
 
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.DurationInt
-import scala.util.Random
 
 class HandlingFailuresSpec extends AnyFeatureSpec with GivenWhenThen {
 
@@ -75,6 +75,124 @@ class HandlingFailuresSpec extends AnyFeatureSpec with GivenWhenThen {
       And ( "I expect the stream will not fail" )
       testProbe.expectMsg ( Done )
       testProbe.expectNoMessage ()
+    }
+
+    Scenario ( "Restart the stream in case of failure" ) {
+
+      Given ( "Given a failing source with restart policy with backoff" )
+
+      var numAttempts = 1
+
+      def simulateFailures ( n: Int ): Int = {
+
+        n % ( 10 * numAttempts ) match {
+
+          case 0 => {
+
+            // TODO TMP
+            println ( s"n: ${n} RuntimeException" )
+
+            numAttempts = numAttempts + 1
+            throw new RuntimeException
+          }
+
+          case _ => {
+
+            // TODO TMP
+            println ( s"n: ${n}" )
+
+            n
+          }
+        }
+      }
+
+      val source = RestartSource.onFailuresWithBackoff (
+
+        minBackoff = 1 second,
+        maxBackoff = 65 seconds,
+        randomFactor = 0.2
+      ) ( () => Source ( 1 to 20 ).map ( value => simulateFailures ( value ) ) )
+
+      val sink = Sink.fold ( 0 ) ( ( a: Int, b: Int ) => a + b )
+
+      When ( "I run the stream" )
+
+      import actorSystem.dispatcher
+
+      val sumFuture: Future [Int] = source
+        .log ( "restartingSource" )
+        .toMat ( sink ) ( Keep.right )
+        .run ()
+        .pipeTo ( testProbe.ref )
+
+      Then ( "I expect it restart after failure with an exponetial backoggthe sum is 445" )
+
+      And ( "The sum is 445 ( first attempt: 1 to 9, second attempt: 1 to 19, third attempt: 1 to 20 )" )
+
+      // First attempt:  1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9
+      // Second attempt: 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17 + 18 + 19
+      // Third attempt:  1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17 + 18 + 19 + 20
+      testProbe.expectMsg ( 5 seconds, 445 )
+    }
+
+    Scenario ( "Restart the stream in case of failure, intercepting values using a TestProbe actor as a sink" ) {
+
+      Given ( "Given a failing source with restart policy with backoff" )
+
+      var numAttempts = 1
+
+      def simulateFailures ( n: Int ): Int = {
+
+        n % ( 5 * numAttempts ) match {
+
+          case 0 => {
+
+            // TODO TMP
+            println ( s"n: ${n} RuntimeException" )
+
+            numAttempts = numAttempts + 1
+            throw new RuntimeException
+          }
+
+          case _ => {
+
+            // TODO TMP
+            println ( s"n: ${n}" )
+
+            n
+          }
+        }
+      }
+
+      val source = RestartSource.onFailuresWithBackoff (
+
+        minBackoff = 1 second,
+        maxBackoff = 60 seconds,
+        randomFactor = 0.2
+      ) ( () => Source ( 1 to 10 ).map ( value => simulateFailures ( value ) ) )
+
+      val flow = Flow [Int].scan [Int] ( 0 ) ( ( a: Int, b: Int ) => a + b )
+
+      val sink = Sink.actorRef ( testProbe.ref, "probe" )
+
+      When ( "I run the stream" )
+
+//      import actorSystem.dispatcher
+
+      source
+        .log ( "restartingSource" )
+        .via ( flow )
+        .to ( sink )
+        .run ()
+
+      Then ( "I expect it restart after failure with an exponetial backoggthe sum is 445" )
+
+      And ( "The sum is 445 ( first attempt: 1 to 9, second attempt: 1 to 19, third attempt: 1 to 20 )" )
+
+      // First attempt:  1 + 2 + 3 + 4
+      // Second attempt: 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9
+      // Third attempt:  1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+      testProbe.expectMsgAllOf ( 5 seconds,0, /* first attempt */ 1, 3, 6, 10,  /* second attempt */ 11, 13, 16, 20, 25, 31, 38, 46, 55,  /* third attempt */  56, 58, 61, 65, 70, 76, 83, 91, 100, 110 )
     }
   }
 }
