4caff4fa70e8911b46fb9700c97959aaebd0cb87
massimo.daros <massimo.daros@external.cerved.com>
2021-02-11 09:30:58 +0100

    Enhanced scenario for and MergeHub

--- src/test/scala/lecture020/MergeHubSpec.scala
+++ src/test/scala/lecture020/MergeHubSpec.scala
@@ -1,8 +1,9 @@
 package lecture020
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{MergeHub, Sink, Source}
+import akka.stream.scaladsl.{Keep, MergeHub, Sink, Source}
 import akka.stream.ActorMaterializer
+import akka.stream.testkit.scaladsl.TestSink
 import org.scalatest.GivenWhenThen
 import org.scalatest.featurespec.AnyFeatureSpec
 
@@ -17,28 +18,26 @@ class MergeHubSpec extends AnyFeatureSpec with GivenWhenThen {
 
   Feature ( "MergeHub" ) {
 
-    Scenario ( "...." ) {
+    Scenario ( "Attach multiple sources using a MergeHub" ) {
 
       Given ( "a MergeHub" )
 
+      val sinkProbe = TestSink.probe [Int]
       val dynamicMerge = MergeHub.source [Int]
-      val materializedSink = dynamicMerge.to ( Sink.foreach [Int] ( println ) ).run ()
+      val ( materializedSink, testSink ) = dynamicMerge.toMat ( sinkProbe ) ( Keep.both ).run ()
 
+      When ( "I attach multiple sources to it" )
 
-      When ( "...." )
-
-      Source ( 1 to 10 ).throttle ( 1, 1 second )
+      Source ( 1 to 10 )
         .runWith ( materializedSink )
 
-      Source ( 91 to 100 ).throttle ( 1, 1 second )
-
+      Source ( 91 to 100 )
         .runWith ( materializedSink )
 
-      Then ( "...." )
+      Then ( "I expect all the messages that will provided by the sources will flow to the MergeHub and reach the subscriber sink" )
 
-      Thread.sleep ( 10 )
-
+      testSink.request ( 20 )
+        .expectNextUnordered ( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100 )
     }
-
   }
 }
